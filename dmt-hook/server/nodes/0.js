310a9651b992518f873be24b5d7cbd4e57394352
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-03 15:00:33 +0000

    rm dmt-hook and add to gitignore

--- dmt-hook/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const entry = 'pages/__layout.svelte-81db7e77.js';
-export const js = ["pages/__layout.svelte-81db7e77.js","chunks/vendor-bcb5457f.js","chunks/index-4478c180.js"];
-export const css = ["assets/pages/__layout.svelte-cf32b02a.css"];
