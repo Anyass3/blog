1c112ed06af73cf198d9e0db7837a76c37245b63
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-03 14:23:16 +0000

    add test blog img

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 
 <div class="w-full h-full flex flex-row flex-wrap justify-between gap-4">
 	{#each data.blogs as { title, pathname }, idx}
-		<div class="bg-gray-900 w-[16rem] h-[16rem] max-w-full aspect-square">
-			<img class='bg-gray-500' src="#" alt="">
+		<div class="bg-gray-900 w-[16rem] h-[16rem] max-w-full aspect-square p-2 rounded-sm transition-transform ease-in-out duration-300 hover:scale-105">
+			<img class='bg-gray-500 aspect-[7/6]' src="/dmt-2.webp" alt="">
 			<p class="text-2xl">
 				<!-- {idx + 1}. -->
 				<a href="/{pathname}" class=" underline">{title}</a>
