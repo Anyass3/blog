be725811b92ccee6741b5ea70d8f171d32e64ade
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-03 14:02:19 +0000

    update app.(css|d.ts|html)

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,32 @@
-/// <reference types="@sveltejs/kit" />
-
-// See https://kit.svelte.dev/docs/typescript
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+// and what to do when importing types
+
 declare namespace App {
-	interface Locals {}
-
-	interface Platform {}
-
-	interface Session {}
-
-	interface Stuff {}
+	// interface Error {}
+	interface Locals {
+		expires: number;
+		token: string;
+		signedToken: string;
+		encryptedData: string;
+		encryptedToken: string;
+		signKeyPair: {
+			signPublicKey: Uint8Array;
+			signSecretKey: Uint8Array;
+			signPublicKeyHex: string;
+			signSecretKeyHex: string;
+		},
+		keyPair:{
+			privateKey: Uint8Array;
+			publicKey: Uint8Array;
+			privateKeyHex: string;
+			publicKeyHex: string;
+		};
+	}
+	// interface PageData {}
+	// interface Platform {}
 }
+
+
+
+// import { SignKeyPair } from '@anyass3/encryption';
