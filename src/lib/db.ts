bba180a031041471bb6333c22c2b259065378042
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-04 13:18:50 +0000

    use ram as storage during building and dev

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,18 @@
+import { HOME } from '$env/static/private';
 import Hyperbee from 'hyperbee';
 import Hypercore from 'hypercore';
+import path from 'path';
+import { dev, building } from '$app/environment';
+import RAM from 'random-access-memory';
 
-const core = new Hypercore('./.db');
+const makeRam = (filename) => {
+	// filename will be one of: data, bitfield, tree, signatures, key, secret_key
+	// the data file will contain all your data concatenated.
+
+	// just store all files in ram by returning a random-access-memory instance
+	return new RAM();
+};
+const core = new Hypercore((building || dev) ? makeRam : path.join(HOME, '/.dmt-here/.db'));
 
 core.ready().then(() => {
 	console.log({ dbKey: core.key });
@@ -28,7 +39,7 @@ export const put = async (title: string, body: string) => {
 	const content = { title, body };
 	const matches = title.match(/[\w]+/g);
 	if (!matches) return;
-	const pathname = matches.join('-');
+	const pathname = matches.join('-').toLowerCase();
 	await db.put(pathname, content);
 	return pathname;
 };
