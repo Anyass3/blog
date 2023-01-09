dc0a0218cc8523b3bdd608a6c969918872311b4b
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-09 00:14:08 +0000

    replace always sending encrypted data with an isAuthenticated bool (reflecting new auth update)

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
+import { authSession } from '$lib/session';
 import type { ServerLoad } from '@sveltejs/kit';
 
-export const load: ServerLoad = ({ locals }) => {
+export const load: ServerLoad = ({ cookies }) => {
 	return {
-		encryptedData: locals.encryptedData
+		isAuthenticated: !!(authSession.has(cookies.get('session') as string))
 	};
 };
--- src/routes/write/+page.ts
+++ src/routes/write/+page.ts
@@ -1,14 +1,16 @@
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import store from '$lib/store';
 import * as E from '@anyass3/encryption';
 import type { PageLoad } from './$types';
+import { browser } from '$app/environment';
 
-export const load: PageLoad = async () => {
+export const load: PageLoad = async ({ parent }) => {
+	const { isAuthenticated } = await parent();
 	const token = store.state.token.get();
 	const publicKey = store.state.publicKey.get();
-	if (!token || !publicKey) throw redirect(303, '/');
-	const encryptedToken = E.encrypt(token, publicKey);
+	if (!(isAuthenticated || token)) throw redirect(303, '/');
+	const encryptedDummy = browser ? E.encrypt(E.random(), publicKey) : '';
 	return {
-		encryptedToken
+		encryptedDummy
 	};
 };
