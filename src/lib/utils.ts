772a6a770a2a85068418021e5b375d908bf22d79
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-09 00:00:52 +0000

    add uploadXHR util

---
+++ src/lib/utils.ts
@@ -0,0 +1,46 @@
+
+type Opts =
+	{
+		onProgress?: (ev: ProgressEvent<XMLHttpRequestEventTarget>) => void,
+		instance?: (xhr: XMLHttpRequest) => void,
+		encryptedDummy: string;
+	}
+
+interface FileResponse {
+	name: string;
+	pathname: string;
+}
+
+export const uploadXHR = (
+	file: File,
+	{ onProgress, instance, encryptedDummy }: Opts
+) => {
+	return new Promise<FileResponse>(
+		(resolve, reject: (value: Record<string, any> | undefined) => void) => {
+			const url = '/file/upload';
+			const xhr = new XMLHttpRequest();
+			if (instance) instance(xhr);
+			const formData = new FormData();
+			xhr.withCredentials = true;
+			xhr.open('POST', url, true);
+			xhr.setRequestHeader('x-encrypted-dummy', encryptedDummy);
+
+			if (onProgress) xhr.upload.addEventListener('progress', onProgress);
+
+			xhr.onload = function (this) {
+				resolve(JSON.parse(this.response || '{}'));
+			};
+
+			xhr.onerror = function (this) {
+				reject(JSON.parse(this.response || '{}'));
+			};
+
+			xhr.onabort = function (this) {
+				reject({ ...JSON.parse(this.response || '{}'), aborted: true });
+			};
+
+			formData.append('file', file);
+			formData.append('tags', '');
+			xhr.send(formData);
+		})
+};
