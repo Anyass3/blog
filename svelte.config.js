03f7eb2c0c8dbb6837b0fb8a7c5815b3d15f2dbc
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-09 00:04:22 +0000

    update packages

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,6 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: vitePreprocess({
 		postcss: true
 	}),
