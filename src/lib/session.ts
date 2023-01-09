aa0515024eb0cbc61558664e5090e6813cf7d1d5
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-09 00:05:13 +0000

    moved authSession to session.ts

---
+++ src/lib/session.ts
@@ -0,0 +1,9 @@
+
+export const authSession = new Map<string, App.Locals>();
+
+export const clearAuthSession = async (session: string) => {
+	
+	if (Date.now() >= (authSession.get(session)?.expires ?? 0)) {
+		authSession.delete(session);
+	}
+};
