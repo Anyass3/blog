bbf5e615938fb82081aa5bc9dcdadd05aceb732d
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-11 23:35:50 +0000

    update blog page, made it faster and more momery efficient and add some meta tags for seo and og

--- src/routes/[blog]/+page.server.ts
+++ src/routes/[blog]/+page.server.ts
@@ -3,10 +3,13 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url }) => {
-	const pathname = url.pathname.split('/').at(-1)?.match(/[\w-]+/)?.[0] ?? '';
-	// console.log(url.pathname);
-	let body = await db.get(pathname);
+	const pathname =
+		url.pathname
+			.split('/')
+			.at(-1)
+			?.match(/[\w-]+/)?.[0] ?? '';
+	const body = await db.get(pathname);
 	if (!body) throw error(404, "Sorry! Blog does't exist.");
-
-	return body;
+	const { content, ...rest } = body;
+	return rest;
 };
--- src/routes/[blog]/+page.svelte
+++ src/routes/[blog]/+page.svelte
@@ -1,14 +1,33 @@
 <script lang="ts">
 	import { base } from '$app/paths';
+	import store from '$lib/store';
 	import Render from '$lib/render.svelte';
 
 	import type { PageData } from './$types';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
+
+	store.commit('writeMode', false);
+
+	// console.log(data);
 </script>
 
-<div class="w-full h-full flex flex-col">
-	<h1 class="text-5xl">{data.title}</h1>
-	<img src="{base}/file/{data.cover}" alt="{data.title}">
-	<Render content={data.content} />
-</div>
+<svelte:head>
+	<title>{data.title} | DMT Blog</title>
+	<meta name="description" content={data.description} />
+	<meta property="og:url" content={$page.url.href} />
+	<meta property="og:title" content="{data.title} | DMT Blog" />
+	<meta
+		property="og:image"
+		content={data.cover ? `${base}/file/${data.cover}` : $page.url.origin + base + '/favicon.png'}
+	/>
+</svelte:head>
+
+<Render
+	contentType="html"
+	content={data.html}
+	date={data.publishedAt}
+	title={data.title}
+	cover={data.cover ? `${base}/file/${data.cover}` : ''}
+/>
