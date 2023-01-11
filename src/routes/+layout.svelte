944f62357a26565df62aba9af247361fafbefd29
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-11 23:30:45 +0000

    update layout with writeMode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
 	import { SnackBar } from 'dmt-gui-kit';
-
+	import store from '$lib/store';
 	import '../app.css';
 	import Nav from '$lib/nav.svelte';
 	import type { PageData } from './$types';
-	import { base } from '$app/paths';
-	import { page } from '$app/stores';
+	export let data: PageData;
 
-	$:console.log(data)
-	export let data: PageData
+	const { writeMode } = store.state;
 
-	const onAuth=(event:Event)=>{
-		if(event.detail){
-			data.isAuthenticated=true;
-		}else data.isAuthenticated=false;
-	}
+	const onAuth = (event: Event) => {
+		if (event.detail) {
+			data.isAuthenticated = true;
+		} else data.isAuthenticated = false;
+	};
 </script>
 
-<main class="flex flex-col justify-center items-center min-h-screen {$page.url.pathname == base + '/write'?'overflow-hidden max-h-screen':''}">
+<main
+	class="flex flex-col justify-center items-center min-h-screen 
+	{$writeMode ? 'overflow-hidden max-h-screen' : ''}"
+>
 	<Nav on:auth={onAuth} isAuthenticated={data.isAuthenticated} />
 	<div class="w-[min(55rem,100%)] px-3  sm:px-0 pt-4 flex-grow relative">
 		<slot />
