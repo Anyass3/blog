84a72105f4ed6f532c19d32feb47819ba7e0d0c1
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-11 23:25:27 +0000

    update stores

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,6 +1,5 @@
 // imports stores-x lib
 import StoreX from 'stores-x';
-import connectome from './connectome';
 import main from './main';
 
-export default StoreX([main, connectome]);
+export default StoreX([main]);
--- src/lib/store/main.ts
+++ src/lib/store/main.ts
@@ -1,17 +1,18 @@
 import * as E from '@anyass3/encryption';
 export default {
-	storeType:{
+	storeType: {
 		publicKey: 'sessionPersistantStore',
 		signPublicKey: 'sessionPersistantStore',
 		token: 'sessionPersistantStore',
 		content: 'sessionPersistantStore',
 		title: 'sessionPersistantStore',
-		cover: 'sessionPersistantStore',
+		cover: 'sessionPersistantStore'
 	},
 	state: {
 		cover: {},
 		content: '',
 		title: '',
+		writeMode: false,
 		navHeight: undefined,
 		metamaskPublicKey: undefined,
 		publicKey: undefined,
