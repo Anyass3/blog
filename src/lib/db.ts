af1dca3d9fdc79ccb9721ea5718a6f09103472a5
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-11 23:19:15 +0000

    update db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,16 +4,25 @@ import Hypercore from 'hypercore';
 import path from 'path';
 import { dev, building } from '$app/environment';
 import RAM from 'random-access-memory';
-import { randomBytes } from 'crypto'
+import { randomBytes } from 'crypto';
 
 export interface Result<T> {
 	seq: number;
 	key: string;
-	value: T
+	value: T;
 }
 
-export type BlogResult = Result<Record<'title' | 'content' | 'cover', string>>
-export type FileResult = Result<Buffer>
+export type BlogResult = Result<
+	Record<'title' | 'html' | 'cover' | 'description' | 'content', string> & { publishedAt: number }
+>;
+export type FileResult = Result<Buffer>;
+export type PutOpts = {
+	title: string;
+	html: string;
+	description: string;
+	content: string;
+	cover?: string;
+};
 
 const makeRam = (filename) => {
 	// filename will be one of: data, bitfield, tree, signatures, key, secret_key
@@ -22,9 +31,8 @@ const makeRam = (filename) => {
 	// just store all files in ram by returning a random-access-memory instance
 	return new RAM();
 };
-const core = new Hypercore((building || dev) ? makeRam : path.join(HOME, '/.dmt-here/.db'));
 
-
+const core = new Hypercore(building || dev ? makeRam : path.join(HOME, '/.dmt-here/.db'));
 
 core.ready().then(() => {
 	console.log({ dbKey: core.key });
@@ -43,22 +51,22 @@ export const blogsDb = db.sub('blogs', {
 export const files = db.sub('files', {
 	keyEncoding: 'utf-8',
 	valueEncoding: 'binary'
-})
+});
 
 export const state = async () => {
-	let state = [];
+	const state: { pathname: string; title: string; cover: string; publishedAt: number }[] = [];
 	for await (const {
 		key,
-		value: { title, cover }
+		value: { title, cover, publishedAt }
 	} of blogsDb.createReadStream()) {
-		state.unshift({ pathname: key, title, cover });
+		state.unshift({ pathname: key, title, cover, publishedAt });
 	}
 	// console.log({ db: state });
 	return state || [];
 };
 
-export const put = async (title: string, content: string, cover?: string) => {
-	const data = { title, content, cover };
+export const put = async ({ title, ...rest }: PutOpts) => {
+	const data = { title, ...rest, publishedAt: Date.now() };
 	const matches = title.match(/[\w]+/g);
 	if (!matches) return;
 	const pathname = matches.join('-').toLowerCase();
@@ -76,16 +84,15 @@ export const del = async (title: string) => {
 	return await blogsDb.del(title);
 };
 
-
 export const saveFile = async (blob: Blob) => {
 	const buf = Buffer.from(await blob.arrayBuffer());
-	const pathname = randomBytes(36).toString('hex')
-	await files.put(pathname, buf)
-	return { pathname }
-}
+	const pathname = randomBytes(36).toString('hex');
+	await files.put(pathname, buf);
+	return { pathname };
+};
 
 export const getFile = async (pathname: string): Promise<FileResult> => {
 	return await files.get(pathname);
-}
+};
 
-export const search = async (query: string) => { };
+export const search = async (query: string) => {};
