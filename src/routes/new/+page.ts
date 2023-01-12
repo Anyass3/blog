391e2f4d29cc0003a5a4091ccdcc558be00bb6eb
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-12 08:38:11 +0000

    add base to root page redirect

--- src/routes/new/+page.ts
+++ src/routes/new/+page.ts
@@ -3,12 +3,13 @@ import store from '$lib/store';
 import * as E from '@anyass3/encryption';
 import type { PageLoad } from './$types';
 import { browser } from '$app/environment';
+import { base } from '$app/paths';
 
 export const load: PageLoad = async ({ parent }) => {
 	const { isAuthenticated } = await parent();
 	const token = store.state.token.get();
 	const publicKey = store.state.publicKey.get();
-	if (!isAuthenticated || !token || !publicKey) throw redirect(303, '/');
+	if (!isAuthenticated || !token || !publicKey) throw redirect(303, base + '/');
 	const encryptedDummy = browser ? E.encrypt(E.random(), publicKey) : '';
 	return {
 		encryptedDummy
